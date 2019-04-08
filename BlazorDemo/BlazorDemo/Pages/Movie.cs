187272b10dda6ca2e06425bc7b6a236a4c35acc2
Jeremy Bailey <jbailey@northernsafety.com>
2019-04-08 15:20:09 -0400

    setup

---
+++ BlazorDemo/BlazorDemo/Pages/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemo.Pages
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Enter a Title"), StringLength(100, ErrorMessage = "That name is too long")]
+        public string Title { get; set; }
+
+        [Required, Range(1920, 2020, ErrorMessage = "Year must be from 1920 to 2020")]
+        public int Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+        public Movie()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ MovieDB/MovieDB.Shared/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDB.Shared
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public Movie()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
