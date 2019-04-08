fcd12f455b26e30fc2a5075fbbb7a44de1513083
Jeremy Bailey <jbailey@northernsafety.com>
2019-04-08 15:40:40 -0400

    migration to 0.9, using EditForm, EventCallBack and DataValidation

--- MovieDB/MovieDB.Shared/Movie.cs
+++ MovieDB/MovieDB.Shared/Movie.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieDB.Shared
 {
     public class Movie
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Enter a Title"), StringLength(100, ErrorMessage = "That name is too long")]
         public string Title { get; set; }
+
+        [Required, Range(1920, 2020, ErrorMessage = "Year must be from 1920 to 2020")]
         public int Year { get; set; }
+
+        [Required]
         public string Director { get; set; }
+
+        [Required]
         public string Description { get; set; }
         public Movie()
         {
